f8116cc8e0ef095800a8986617821cb6fa980f94
apat20 <apat201094@gmail.com>
2019-05-19 20:14:28 -0400

    Separate functions for computing the rotation matrices for manipulators under consideration

---
+++ EulerRotation.m
@@ -0,0 +1,5 @@
+%This function is used to calculate the rotation matrix using the Euler
+%angle and fixed angle representation 
+
+function rotation_matrix = EulerRotation[alpha, beta, gamma]
+end 
---
+++ test_script.m
@@ -0,0 +1,6 @@
+%this is the test script
+
+theta = deg2rad(30);
+axis = 'y';
+rotation_matrix = RotationMatrix(theta,axis);
+
