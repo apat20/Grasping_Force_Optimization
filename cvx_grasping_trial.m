16096ab7dc05633ab03c9c3ceb41b63586120e8b
apat20 <apat201094@gmail.com>
2019-06-05 16:05:57 -0400

    Minor changes

--- cvx_grasping_trial.m
+++ cvx_grasping_trial.m
@@ -17,3 +17,44 @@ G = [R_1, R_2;
 
 disp(G);
 
+%Contact forces considering point 
+%contact with friction
+%Get the values of the contact force vectors directly
+%from the user
+fc_1 = ['fx_1';
+        'fy_1';
+        'fz_1'];
+
+fc_2 = ['fx_2';
+        'fy_2';
+        'fz_2'];
+
+%Concatenated force vector
+B = [fc_1; fc_2];
+
+%Force equilibrium condition for grasping
+F_external = -G*B;
+
+%Specifying the friction coefficient
+f_coeff = 0.1;
+
+disp(B);
+
+%Getting the force F as expressed in the conic 
+%formulation of the problem in Boyd's paper.
+
+F = ['F_x';
+     'F_y';
+     'F_z'];
+ 
+%Writing the optimisation code
+cvx_begin
+    variable F
+    minimize(norm(F))
+cvx_end
+
+
+
+
+
+
