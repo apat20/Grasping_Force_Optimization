43bf269c5810f9ce22539501e8c445e3cb892e97
apat20 <apat201094@gmail.com>
2019-06-10 14:05:29 -0400

    Minor changes to the Grasp Map computing function

--- GraspMap.m
+++ GraspMap.m
@@ -1,16 +1,28 @@
 %This function can be used to compute the Grasp map for a single finger
 %using the wrench basis for soft point contact
 
-function G = GraspMap(R, p_hat)
-    B_c = [1,0,0,0
-           0,1,0,0;
-           0,0,1,0;
-           0,0,0,0;
-           0,0,0,0;
-           0,0,0,1];
-   
-    Adjoint = [R      , zeros(3);
-               p_hat*R, R];
+function G = GraspMap(R, p_hat, x)
+    if x == 'SF'
+        B_c = [1,0,0,0
+              0,1,0,0;
+              0,0,1,0;
+              0,0,0,0;
+              0,0,0,0;
+              0,0,0,1];
+         Adjoint = [R      , zeros(3);
+                    p_hat*R, R];
            
-    G = Adjoint*B_c;
+         G = Adjoint*B_c;
+    elseif x == 'PF'
+        B_c = [1,0,0;
+              0,1,0;
+              0,0,1;
+              0,0,0;
+              0,0,0;
+              0,0,0];
+         Adjoint = [R      , zeros(3);
+                    p_hat*R, R];
+           
+         G = Adjoint*B_c;
+    end
 end
--- cvx_trial.m
+++ cvx_trial.m
@@ -1,11 +1,29 @@
-m = 16; n = 8;
-A = randn(m,n);
-b = randn(m,1);
+%The rotation matrices for our selected problem
+R_1 = [0,1,0;
+       0,0,1;
+       1,0,0];
+R_2 = [1,0,0;
+       0,0,-1;
+       0,1,0];
 
-cvx_begin
-    variable x(n)
-    minimize(norm(A*x-b))
-cvx_end
-
-disp(cvx_optval);
-disp(cvx_status);
+%The position vectors in the skew symmetric form for our selected problem
+p1_hat = [0,0,2;
+          0,0,0;
+         -2,0,0];
+p2_hat = [0,0,-2;
+          0,0,0;
+          2,0,0]; 
+B_c = [1,0,0;
+        0,1,0;
+        0,0,1;
+        0,0,0;
+        0,0,0;
+        0,0,0];
+ Adjoint = [R_1      , zeros(3);
+            p1_hat*R_1, R_1];
+           
+ G = Adjoint*B_c;
+ disp(G);
+ 
+ 
+ 
